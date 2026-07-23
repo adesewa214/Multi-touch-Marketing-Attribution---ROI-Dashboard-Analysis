837d4a2170e7898976f2b223d6b8e646a16776cd
Adesewa <sisimiadesewa@gmail.com>
2026-07-23 20:09:52 +0100

    edited the average revenue per customer query

--- Sql/Average_revnue_per_customer.sql
+++ Sql/Average_revnue_per_customer.sql
@@ -1,3 +1,7 @@
-SELECT ROUND(SUM(Revenue_USD), 2)
-AS Revenue_Per_Customer 
-FROM dbo.Multi_touch_market_attribution
+SELECT
+ROUND(
+SUM(Revenue_USD)
+/ COUNT(DISTINCT User_ID),
+2
+) AS Revenue_Per_Customer
+FROM dbo.Multi_touch_market_attribution;
